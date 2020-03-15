03a62289f6f4f2e775b8e4283fbc30257ac0177c
jimenezict <jimenezict@gmail.com>
2020-03-15 19:15:03 +0100

    first code

---
+++ src/main/java/com/uoc/tfm/controller/ConsumerController.java
@@ -0,0 +1,12 @@
+package com.uoc.tfm.controller;
+
+import com.uoc.tfm.service.ConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConsumerController {
+
+    @Autowired
+    private ConsumerService consumerService;
+
+
+}
---
+++ src/main/java/com/uoc/tfm/service/ConsumerService.java
@@ -0,0 +1,9 @@
+package com.uoc.tfm.service;
+
+public interface ConsumerService {
+
+    public void getStations();
+
+    public void getStationStatus();
+
+}
