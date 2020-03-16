333025f524eab107507a5fa8f39810ba2d83a894
jimenezict <jimenezict@gmail.com>
2020-03-16 23:24:49 +0100

    Finalizing Domain

--- src/main/java/com/uoc/tfm/domain/Station.java
+++ src/main/java/com/uoc/tfm/domain/Station.java
@@ -1,26 +1,48 @@
 package com.uoc.tfm.domain;
 
-public class Station {
+class Station {
 
-    private final int id;
-    private final int size;
-    private final int occupacy;
+    private int id;
+    private double x_coordinate;
+    private int y_coordinate;
+    private String address;
 
-    public Station(int id, int size, int occupacy) {
+    public Station(int id, double x_coordinate, int y_coordinate, String address) {
         this.id = id;
-        this.size = size;
-        this.occupacy = occupacy;
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
+        this.address = address;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getSize() {
-        return size;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getOccupacy() {
-        return occupacy;
+    public double getX_coordinate() {
+        return x_coordinate;
+    }
+
+    public void setX_coordinate(double x_coordinate) {
+        this.x_coordinate = x_coordinate;
+    }
+
+    public int getY_coordinate() {
+        return y_coordinate;
+    }
+
+    public void setY_coordinate(int y_coordinate) {
+        this.y_coordinate = y_coordinate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
