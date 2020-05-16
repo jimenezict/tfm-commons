184d91f2ec5cc473fc2c8e449474a311f8ee27bc
LENOVO-PC\Daniel <jimenezict@gmail.com>
2020-05-16 16:43:00 +0200

    Avoiding exceptions on the call

--- src/main/java/com/uoc/tfm/controller/ClientControllerImpl.java
+++ src/main/java/com/uoc/tfm/controller/ClientControllerImpl.java
@@ -6,6 +6,7 @@ import com.uoc.tfm.service.ClientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +20,14 @@ public class ClientControllerImpl implements ClientController {
     @Override
     public StationsLocation getStationsLocation() {
         log.info("Starting station collections: {}", clientService.getServiceName());
-        StationsLocation stationsLocation = clientService.getStationsLocation();
+        StationsLocation stationsLocation = new StationsLocation();
+
+        try {
+            stationsLocation = clientService.getStationsLocation();
+        } catch(Exception e) {
+            log.error("Fail on Location status collection {} due to: ", clientService.getServiceName(), e);
+            return stationsLocation;
+        }
         log.info("Ending station collections: {} with {} registers",
                 clientService.getServiceName(),
                 stationsLocation.getStationLocationList().size());
@@ -28,9 +36,17 @@ public class ClientControllerImpl implements ClientController {
     }
 
     @Override
+    @GetMapping("/status")
     public StationsStatus getStationStatus() {
         log.info("Starting station status collections: {}", clientService.getServiceName());
-        StationsStatus stationStatus = clientService.getStationStatus();
+        StationsStatus stationStatus = new StationsStatus();
+
+        try {
+            stationStatus = clientService.getStationStatus();
+        } catch(Exception e) {
+            log.error("Fail on station status collection {} due to: ", clientService.getServiceName(), e);
+            return stationStatus;
+        }
         log.info("Ending station status collections: {} with {} registers",
                 clientService.getServiceName(),
                 stationStatus.getStationStatusList().size());
